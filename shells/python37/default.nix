cec363113b0f1a22e6194909bad9166833f6574c
yosephbernandus <yosephbernandus@gmail.com>
2025-05-05 10:18:10 +0700

    add locale path

--- shells/python37/default.nix
+++ shells/python37/default.nix
@@ -2,6 +2,7 @@
 pkgs.mkShell {
   buildInputs = [
     python37
+    pkgs.glibcLocales # Add this for locale support
   ];
   
   shellHook = ''
@@ -12,6 +13,11 @@ pkgs.mkShell {
     
     # Export PATH to ensure Python 3.7 is first
     export PATH=${python37}/bin:$PATH
+    
+    # Fix locale issues
+    export LOCALE_ARCHIVE=${pkgs.glibcLocales}/lib/locale/locale-archive
+    export LANG=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
     
     echo "Python 3.7 is ready!"
     echo "Run 'python' to start the interpreter"
